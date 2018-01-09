e9e6d3041edbe43f22e38025835459193292ca8d
Mottaqui Karim <mottaquikarim@users.noreply.github.com>
2018-01-08 22:08:49 -0500

    Add files via upload

---
+++ Lecture_10/class_notes/selectors_review_vanilla/js/selectorsReview.js
@@ -0,0 +1,45 @@
+(function() { // protect the lemmings!
+
+	// the goal of this pset is to select different HTML elements with JQuery
+	// it is meant to show you a little bit about the paradigm jQuery is built around
+	// and demonstrate the power of the jQuery selector engine ( called sizzle )
+
+	// please select the item with ID : page-header
+	// save it to a variable and console.log it
+	var pageHeaderEl;
+
+	// please select the item or items with CLASS : card
+	// save it to a variable and console.log it
+	var cardEl;
+	// what kind of javascript variable is cardEl?
+	var cardElType;
+	// (interesting article that describes what the jQuery object actually returns:
+	// http://www.learningjquery.com/2008/12/peeling-away-the-jquery-wrapper/ )
+
+	// please select all the unorder lists in the document
+	// save it as var and console log it
+	var ulEls;
+
+	// I want to know how many anchor tags are present in the ENTIRE document
+	// please construct a selector that will grab them all and console.log out
+	// the number of anchor tags in this document
+	var numAnchorTags;
+
+	// I would like to know how many ULs can be found inside the CLASS: card
+	// please construct a selector that chooses them all and console.log out the number
+	// of ULs that are children of the CLASS: card
+	var numberOfUlsChildrenOfLIs;
+
+	// please console.log out the text inside the FIRST list item in ID: page-header
+	var textInsideFirstPageHeaderItem;
+
+	// please construct a selector that returns ALL the ODD list items in
+	// the HTML element with CLASS: cards
+	// console.log out these results
+	var oddChildrenOfCards;
+
+
+	// REFERENCE: here's a list of jQuery selectors:
+	// http://api.jquery.com/category/selectors/
+
+})();
